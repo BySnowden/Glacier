96a7d01c59a2765436418ff99fbbe41f4b416c5c
BySnowden <tristansnow20@gmail.com>
2026-01-03 18:24:47 -0600

    Updated Window Name

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	err = wails.Run(&options.App{
-		Title:            "Glacier",
+		Title:            "Glacier Mod Manager",
 		Width:            1024,
 		Height:           768,
 		WindowStartState: options.Maximised,
